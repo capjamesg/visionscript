cb6bf34bc72fed6342e3619f981d8d288de42321
James Gallagher <jamesg@jamesg.blog>
2023-07-06 00:17:24 +0100

    add test cases

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-from lang import parse_tree
+import lang
 import os
 
 TEST_DIR = "./tests/"
 
 for file in os.listdir(TEST_DIR):
+    lang.state = lang.init_state()
     if file.endswith(".vic"):
         print(f"Testing {file}...")
         
-        with open(TEST_DIR + file, "r") as f:
-            parse_tree(f.read())
+        try:
+            with open(TEST_DIR + file, "r") as f:
+                lang.parse_tree(lang.parser.parse(f.read().strip()))
+        except Exception as e:
+            print(f"Test {file} failed!")
+            print(e)
+            continue
 
         print(f"Test {file} passed!")
