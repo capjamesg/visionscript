ce9b2a8f48e963cedbe3e4feda4a3b0ee361af1d
James Gallagher <jamesg@jamesg.blog>
2023-07-06 00:00:46 +0100

    work on train, compare, read, label, list, and get statements

--- grammar.py
+++ grammar.py
@@ -1,10 +1,11 @@
 grammar = """
 start: expr+
 
-expr: classify | replace | load | save | say | detect | cutout | size | count | countinregion | in | if | segment | BOOL | EQUALITY | var | EOL | variable | comment | show | exit | help | train | INT
+expr: classify | replace | load | save | say | detect | cutout | size | count | countinregion | in | if | segment | BOOL | EQUALITY | var | EOL | variable | comment | show | exit | help | train | compare | read | label | use | list | get | INT
 classify: "Classify" "[" STRING ("," STRING)* "]"
 var: variable "=" expr
 replace: "Replace" "[" STRING "]"
+use: "Use" "[" STRING "]"
 load: "Load" "[" STRING "]" | "Load" "[" "]"
 save: "Save" "[" STRING "]"
 say: "Say" "[" "]"
@@ -12,7 +13,11 @@ size: "Size" "[" "]"
 show: "Show" "[" "]"
 cutout: "Cutout" "[" "]"
 count: "Count" "[" "]"
+compare: "Compare" "[" "]"
+read: "Read" "[" "]"
 exit: "Exit" "[" "]"
+get: "Get" "[" INT "]" EOL
+list: "[" ((STRING | INT | expr) "," | (STRING | INT | expr) )* "]" EOL
 help: "Help" "[" STRING "]"
 countinregion: "CountInRegion" "[" INT "," INT "," INT "," INT "]"
 detect: "Detect" "[" STRING ("," STRING)* "]" | "Detect" "[" "]"
@@ -21,7 +26,8 @@ in: "IN" "[" STRING "]" EOL (INDENT expr+)*
 if: "IF" "[" (expr+) "]" EOL (INDENT expr+)*
 OPERAND: "+" | "-" | "*" | "/"
 EQUALITY: "=="
-train: "Train" "[" STRING "]"
+train: "Train" "[" STRING "," STRING "]" | "Train" "[" STRING "]"
+label: "Label" "[" STRING "," STRING ("," STRING )*  "]" 
 variable: /[a-zA-Z]+/
 comment: /#.*$/ (expr)*
 EOL: "\\n"
--- lang.py
+++ lang.py
@@ -3,6 +3,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import logging
+import math
 import mimetypes
 import optparse
 import os
@@ -10,30 +11,33 @@ import random
 import string
 import sys
 import tempfile
+from collections import deque
 
 import cv2
 import numpy as np
 import supervision as sv
 from lark import Lark, UnexpectedCharacters, UnexpectedToken
 from PIL import Image
-from spellchecker import SpellChecker
 
-from usage import (
-    USAGE,
-    language_grammar_reference,
-    lowercase_language_grammar_reference,
-)
 from grammar import grammar
+from usage import (USAGE, language_grammar_reference,
+                   lowercase_language_grammar_reference)
+
+# from spellchecker import SpellChecker
+
 
 state = {
     "last": None,
     "last_function_type": None,
     "last_function_args": None,
     "last_loaded_image": None,
+    "image_stack": [],
+    "detections_stack": [],
     "history": [],
+    "current_active_model": None
 }
 
-spell = SpellChecker()
+# spell = SpellChecker()
 
 opt_parser = optparse.OptionParser()
 
@@ -57,6 +61,12 @@ else:
 if options.file is not None:
     with open(options.file, "r") as f:
         code = f.read()
+
+code = """
+x = ["Yay", "Yayyy!"]
+Get[1]
+Say[]
+"""
 
 parser = Lark(grammar)
 
@@ -109,19 +119,23 @@ def handle_unexpected_token(e):
 
 
 if options.validate:
-    print("Script is a valid VisualScript program.")
+    print("Script is a valid VisionScript program.")
     exit(0)
 
 
 def literal_eval(string):
     return string[1:-1]
 
+def set_state(key, value):
+    state[key] = value
+
+def use(model_name):
+    set_state("current_active_model", model_name)
+
 
 def load(filename):
-    if "requests" not in sys.modules:
-        import requests
-    if "validators" not in sys.modules:
-        import validators
+    import requests
+    import validators
 
     if validators.url(filename):
         response = requests.get(filename)
@@ -150,7 +164,7 @@ def load(filename):
 
     state["last_loaded_image_name"] = filename
 
-    return Image.open(filename)
+    return np.array(Image.open(filename).convert("RGB"))[:, :, ::-1]
 
 
 def size(_):
@@ -160,7 +174,9 @@ def size(_):
 def cutout(_):
     x1, y1, x2, y2 = state["last"].xyxy[0]
     image = state["last_loaded_image"]
-    state["last_loaded_image"] = image.crop((x1, y1, x2, y2))
+    cropped_image = image.crop((x1, y1, x2, y2))
+    state["image_stack"].append(cropped_image)
+    state["last_loaded_image"] = cropped_image
 
 
 def save(filename):
@@ -177,8 +193,7 @@ def count(args):
 def detect(classes):
     logging.disable(logging.CRITICAL)
 
-    if "ultralytics" not in sys.modules:
-        from ultralytics import YOLO
+    from ultralytics import YOLO
 
     model = YOLO("yolov8n.pt")
     inference_results = model(state["last_loaded_image"])[0]
@@ -196,6 +211,8 @@ def detect(classes):
     classes = [key for key, item in inference_classes.items() if item in classes]
 
     results = results[np.isin(results.class_id, classes)]
+
+    state["detections_stack"].append(results)
 
     return results
 
@@ -303,12 +320,16 @@ def countInRegion(x1, y1, x2, y2):
     return counter
 
 
+def read(_):
+    return state["last"]
+
+
 def say(statement):
     if state.get("last_function_type", None) in ("detect", "segment"):
         last_args = state["last_function_args"]
         statement = "".join(
             [
-                f"{last_args[i]} {state['last'].confidence[i]:.2f} {state['last'].xyxy[i]}\n"
+                f"{last_args} {state['last'].confidence[i]:.2f} {state['last'].xyxy[i]}\n"
                 for i in range(len(state["last"].xyxy))
             ]
         )
@@ -337,17 +358,38 @@ def replace(filename):
     state["last_loaded_image"].paste(random_img, (int(xyxy[0][0]), int(xyxy[0][1])))
 
 
-def train(folder):
+def label(args):
+    folder = args[0]
+    model = args[1]
+    items = args[2]
     # if Detect or Classify run, train
-    if "Detect" in state["history"]:
+    if "Detect" in state["history"] or state["current_active_model"] == "Segment":
+        from autodistill.detection import CaptionOntology
+        from autodistill_grounded_sam import GroundedSAM
+
+        mapped_items = {item: item for item in items}
+
+        base_model = GroundedSAM(CaptionOntology(mapped_items))
+    else:
+        print("This model doesn't exist yet!")
+        return
+    
+    base_model.label(folder)
+
+
+def train(args):
+    folder = args[0]
+    model = args[1]
+    # if Detect or Classify run, train
+    if "Detect" in state["history"] or model == "yolov8":
         if "autodistill_yolov8" not in sys.modules:
-            import autodistill_yolov8 as YOLOv8
+            from autodistill_yolov8 import YOLOv8
 
         base_model = YOLOv8("yolov8n.pt")
 
-        model = base_model.train(folder, "yolov8n.pt")
+        model = base_model.train(os.path.join(folder, "data.yaml"), epochs=10)
 
-    elif "Classify" in state["history"]:
+    elif "Classify" in state["history"] or model == "vit":
         if "autodistill_vit" not in sys.modules:
             import autodistill_vit as ViT
 
@@ -375,6 +417,25 @@ def show(_):
         print("Image does not exist.")
         return
 
+    if state.get("history", [])[-1] == "compare":
+        images = []
+
+        grid_size = math.gcd(len(state["image_stack"]), len(state["image_stack"]))
+
+        for image, detections in zip(state["image_stack"], state["detections_stack"]):
+            if annotator and detections:
+                image = annotator.annotate(np.array(image), detections)
+            else:
+                image = np.array(image)
+
+            images.append(image)
+
+        sv.plot_images_grid(
+            images=np.array(images), grid_size=(grid_size, grid_size), size=(12, 12)
+        )
+
+        return
+
     if annotator:
         image = annotator.annotate(
             cv2.imread(state["last_loaded_image_name"]), state["last"]
@@ -384,6 +445,9 @@ def show(_):
 
     sv.plot_image(image, (8, 8))
 
+def get_func(x):
+    print(x)
+    state["last"] = state["last"][x]
 
 # if None, the logic is handled in the main parser
 function_calls = {
@@ -401,32 +465,49 @@ function_calls = {
     "in": lambda x: None,
     "if": lambda x: None,
     "var": lambda x: None,
+    "variable": lambda x: None,
     "comment": lambda x: None,
     "expr": lambda x: None,
     "show": lambda x: show(x),
     "exit": lambda x: exit(0),
     "help": lambda x: print(language_grammar_reference[x]),
     "train": lambda x: train(x),
+    "compare": lambda x: show(x),
+    "read": lambda x: read(x),
+    "label": lambda x: label(x),
+    "list": lambda x: None,
+    "get": lambda x: get_func(x)
 }
 
 
 def parse_tree(tree):
+    # print(tree)
     if not hasattr(tree, "children"):
         if hasattr(tree, "value") and tree.value.isdigit():
             return int(tree.value)
+        elif isinstance(tree, str):
+            return literal_eval(tree)
 
     for node in tree.children:
+        print(node)
         # ignore EOLs, etc.
+        # if node is a list, parse it
+        # print all tree attrs
         if node == "True":
             return True
         elif node == "False":
             return False
+        elif node.data == "list":
+            node = node
         elif (hasattr(node, "type") and node.type == "INT") or isinstance(node, int):
             return int(node.value)
         elif not hasattr(node, "children") or len(node.children) == 0:
             node = node
         elif state.get("ctx") and (state["ctx"].get("in") or state["ctx"].get("if")):
             node = node
+        # if string
+        elif len(node.children) == 1 and hasattr(node.children[0], "value"):
+            return node.children[0].value
         else:
             node = node.children[0]
 
@@ -447,6 +528,14 @@ def parse_tree(tree):
 
         if token.type == "EQUALITY":
             return parse_tree(node.children[0]) == parse_tree(node.children[1])
+
+        if token == "list":
+            results = []
+
+            for item in node.children:
+                results.append(parse_tree(item))
+
+            return results
 
         if token == "var":
             state[node.children[0].children[0].value] = parse_tree(node.children[1])
@@ -475,6 +564,9 @@ def parse_tree(tree):
             continue
 
         func = function_calls[token.value]
+
+        if token.value == "get":
+            continue
 
         state["history"].append(token.value)
 
@@ -526,13 +618,14 @@ def parse_tree(tree):
         state["last_function_args"] = [value]
 
         if token.value == "load":
+            state["image_stack"].append(result)
             state["last_loaded_image"] = result
 
 
 if options.repl:
-    print("Welcome to VisualScript!")
+    print("Welcome to VisionScript!")
     print("Type 'Exit[]' to exit.")
-    print("Read the docs at https://visualscript.org/docs")
+    print("Read the docs at https://visionscript.org/docs")
     print("For help, type 'Help[FunctionName]'.")
     print("-" * 20)
     while True:
@@ -559,4 +652,9 @@ if __name__ == "__main__":
     if DEBUG:
         print(tree.pretty())
 
-    parse_tree(tree)
+    try:
+        parse_tree(tree)
+    except KeyboardInterrupt:
+        print("Exiting VisionScript.")
+
+    print(state)
