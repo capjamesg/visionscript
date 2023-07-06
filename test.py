81517a2988841b506e69c8a6468907f190599354
capjamesg <jamesg@jamesg.blog>
2023-07-07 00:37:20 +0100

    fix test suite, add color feature to replace[], update tests

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-import lang
+import visionscript as lang
 import os
 
 TEST_DIR = "./tests/"
 
 for file in os.listdir(TEST_DIR):
-    lang.state = lang.init_state()
+    session = lang.VisionScript()
     if file.endswith(".vic"):
         print(f"Testing {file}...")
         
         try:
             with open(TEST_DIR + file, "r") as f:
-                lang.parse_tree(lang.parser.parse(f.read().strip()))
+                session.parse_tree(lang.parser.parse(f.read() + "\n"))
         except Exception as e:
             print(f"Test {file} failed!")
             print(e)
